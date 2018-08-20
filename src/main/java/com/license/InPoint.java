a5fef90c9f2d1e7b3bb93b8a6535b61ea1c1305e
WuChenghao <WuChenghao@DESKTOP-B6AA7HD>
2018-08-20 13:13:29 +0800

    setting the fileinput and the java inpoint

--- src/main/java/com/license/InPoint.java
+++ src/main/java/com/license/InPoint.java
@@ -1,9 +1,24 @@
 package com.license;
 
-public class InPoint {
+import org.mule.api.MuleEventContext;
+import org.mule.api.MuleMessage;
+import org.mule.api.lifecycle.Callable;
+
+public class InPoint  implements Callable{
 
 	public static void main(String[] args) {
 		System.out.println("running success");
+	}
+
+	@Override
+	public Object onCall(MuleEventContext eventContext) throws Exception {
+		
+		MuleMessage message = eventContext.getMessage();
+
+		// get file original name
+		String fileName = message.getOutboundProperty("originalFilename");
+		System.out.println(fileName);
+		return null;
 	}
 
 }
